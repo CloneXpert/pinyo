9263d0a60ca46d3eb6425a3ec990b840747899d3
CloneXpert <cl_0nexpert@yahoo.com>
2015-03-28 10:39:56 +0100

    Use methods for creating teams

--- client/views/csapatok/newTeam.js
+++ client/views/csapatok/newTeam.js
@@ -7,7 +7,16 @@ Template.newTeam.events({
       address: $(e.target).find('[name=address]').val()
     };
 
-    team._id = Teams.insert(team);
-    Router.go('teamPage', team);
+    Meteor.call('newTeam', team, function (error, result) {
+      if (error) {
+        return alert(error.reason);
+      }
+
+      if(result.teamExists) {
+        alert('A csapat már létre lett hozva');
+      }
+
+      Router.go('teamPage', {_id: result._id});
+    });
   }
 });
--- collections.js
+++ collections.js
@@ -1,3 +1,2 @@
 Seasons = new Meteor.Collection("seasons");
-Teams = new Meteor.Collection("teams");
 Rounds = new Meteor.Collection("rounds");
