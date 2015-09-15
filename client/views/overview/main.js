e27efd73d000e313a3b5c40a093ce30c56ae0c05
CloneXpert <cl_0nexpert@yahoo.com>
2015-09-15 21:55:47 +0200

    Added moment hu locale and fixed date formats

--- client/views/overview/main.js
+++ client/views/overview/main.js
@@ -1,7 +1,11 @@
 myTeamName = "Morgan Stanley SK I.";
 
-Template.registerHelper('formatDate', function(date) {
+Template.registerHelper('formatDateWithTime', function(date) {
   return moment(date).format('LLLL');
+});
+
+Template.registerHelper('formatDate', function(date) {
+  return moment(date).format('LL');
 });
 
 Template.registerHelper('Schemas', Schemas);
